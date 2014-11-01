eadb5ee2fbbabad9c8ebbe1d003835e160dd17c3
Celestine Omin <cyberomin@yahoo.com>
2014-11-01 22:57:55 +0100

    Recursively solve for only valid input

--- sudoku.py
+++ sudoku.py
@@ -1,72 +1,132 @@
+"""Sudoko Solver
+Description: The Sudoku solver Accepts a sudoku puzzle in CSV format and
+recursively adds a valid input to the unsolved location
+until the puzzle has reached a valid solution.
+It returns a CSV as its result.
+"""
 from __future__ import division
-import sys
-import string
 import csv
 
 
-class Sudoku:
+class Sudoku(object):
+    """A sudoku solver that uses a recursive strategy"""
 
     def __init__(self):
         """Initialize the puzzle to solve"""
-        pass
+        self.rows = 9
+        self.cols = 9
 
-    def parse_csv(self, csv_file):
+    @staticmethod
+    def parse_csv(csv_file):
         """Parse CSV gotten from the constructor and generates
         a string of numbers."""
-        with open(csv_file, 'r') as f:
-            lines = csv.reader(f)
+        with open(csv_file, 'r') as file_to_read:
+            lines = csv.reader(file_to_read)
             data = [numbers for line in lines for numbers in line]
 
         parsed_result = "".join(data)
         return parsed_result
 
-    def solved_csv(self, result):
+    @staticmethod
+    def write_to_csv(result):
         """Writes the result of the puzzle in a CSV file."""
-        data = []
         solved_file = "solution.csv"
-        result = str(result)
-        for i in range(0, len(result), 9):
-            data.append(list(result[i:i + 9]))
+        with open(solved_file, 'w') as file_to_process:
+            writer = csv.writer(file_to_process, delimiter=',')
+            writer.writerows(result)
 
-        with open(solved_file, 'w') as fp:
-            a = csv.writer(fp, delimiter=',')
-            a.writerows(data)
+    @staticmethod
+    def group(puzzle, num):
+        """Arrange the puzzle as an n x n matrix"""
+        result = []
+        grid = [puzzle[i:i+num] for i in range(0, len(puzzle), num)]
+        for line in grid:
+            result.append([int(y) for y in line])
+        return result
 
-    def same_row(self, i, j):
-        """Checks to see if the rows are the same"""
-        return i/9 == j/9
+    def find_unsolved_location(self, grid):
+        """Find unsolved location across the matirx"""
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if grid[row][col] == 0:
+                    return (row, col)
 
-    def same_col(self, i, j):
-        """Checks to see if the column are the same"""
-        return (i-j) % 9 == 0
+    @staticmethod
+    def get_row(grid, pos):
+        """Get the row element for the current position
+        1 2 3 4 5 6 7 8 9
+        """
+        row = pos[0]
+        return [x for x in grid[row]]
 
-    def same_block(self, i, j):
-        return i/27 == j/27 and i % 9/3 == j % 9/3
+    def get_col(self, grid, pos):
+        """Get the column element for the current position
+        1
+        2
+        3
+        4
+        5
+        6
+        7
+        8
+        9
+        """
+        row, col = pos
+        return [grid[row][col] for row in range(self.rows)]
 
-    def solve(self, puzzle):
-        """Solves the puzzle and returns a solution as CSV"""
-        i = puzzle.find('0')
-        if i == -1:
-            self.solved_csv(puzzle)
-            sys.exit(puzzle)
-        excluded_numbers = set()
-        for j in range(81):
-            if self.same_row(i, j) \
-                    or self.same_col(i, j) \
-                    or self.same_block(i, j):
-                excluded_numbers.add(puzzle[j]) # excluding the number zero from the list of possible numbers
+    @staticmethod
+    def get_block(grid, pos):
+        """Get the block for the current position
+        1 2 3
+        4 5 6
+        7 8 9
+        """
+        row, cow = pos
+        br0 = 3 * (row // 3)
+        bc0 = 3 * (cow // 3)
+        return [grid[br0 + r][bc0 + c] for r in range(3) for c in range(3)]
 
-        for m in string.digits[1:]:
-            if m not in excluded_numbers:
-                self.solve(puzzle[:i] + m + puzzle[i+1:])
+    def find_possible_values(self, grid, pos):
+        """Find a possible value for the unsolved location"""
+        return set(range(1, 10)) - \
+            set(self.get_row(grid, pos)) - \
+            set(self.get_col(grid, pos)) - \
+            set(self.get_block(grid, pos))
 
-if __name__ == '__main__':
+    def solve(self, grid):
+        """Solve sudoku puzzle"""
+        pos = self.find_unsolved_location(grid)
+        solution = ""
+        if not pos:
+            return grid
+        row, col = pos
+        for value in self.find_possible_values(grid, pos):
+            # Number hasn't been used across row and column
+            if self.is_legal(value, grid, pos):
+                grid[row][col] = int(value)
+            solution = self.solve(grid)
+        if solution:
+            return solution
+        grid[row][col] = 0  # could not find a solution, fix in a zero there.
+
+    def is_legal(self, value, grid, pos):
+        """Check if the value can fit into the current position"""
+        return value not in self.get_row(grid, pos) and \
+            self.get_col(grid, pos) and \
+            self.get_block(grid, pos)
+
+
+if __name__ == "__main__":
+    import sys
+
     if len(sys.argv) == 2 and sys.argv[1].endswith(".csv"):
         sudoku = Sudoku()
-        puzzle = sudoku.parse_csv(sys.argv[1])
-        sudoku.solve(puzzle)
+        sudoku_num = sudoku.parse_csv("puzzle.csv")
+        matrix = sudoku.group(sudoku_num, 9)
+        answer = sudoku.solve(matrix)
+        sudoku.write_to_csv(answer)
         print "Solved. The result is in solution.csv"
     else:
         print 'Usage: python sudoku.py puzzle.csv'
         print 'The puzzle.csv is a 9 x 9 file where 0 represents ' \
-              'unsolved location or blanks.'
+            'unsolved location or blanks.'
