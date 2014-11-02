442892e3b6a90f4399590400df5f73f0511e3926
Celestine Omin <cyberomin@yahoo.com>
2014-11-02 12:56:16 +0100

    Refactored test cases

--- sudoku_test.py
+++ sudoku_test.py
@@ -1,20 +1,19 @@
 import unittest
-from sudoku import Sudoku
 import os
+from sudoku import Sudoku
 
 
 class SudokuTest(unittest.TestCase):
 
     def setUp(self):
         self.sudoku = Sudoku()
+        self.sudoku_puzzle = self.sudoku.parse_csv("puzzle.csv")
 
     def test_puzzle_has_eighty_one_items(self):
-        sudoku_puzzle = self.sudoku.parse_csv("puzzle.csv")
-        self.assertEquals(len(sudoku_puzzle), 81)
+        self.assertEquals(len(self.sudoku_puzzle), 81)
 
     def test_solve_puzzle(self):
-        sudoku_num = self.sudoku.parse_csv("puzzle.csv")
-        matrix = self.sudoku.group(sudoku_num, 9)
+        matrix = self.sudoku.group(self.sudoku_puzzle, 9)
         answer = self.sudoku.solve(matrix)
         self.sudoku.write_to_csv(answer)
         dir = os.curdir
@@ -22,8 +21,8 @@ class SudokuTest(unittest.TestCase):
         self.assertIn("solution.csv", file_sys)
 
     def test_solution_has_eighty_one_items(self):
-        sudoku_puzzle = self.sudoku.parse_csv("solution.csv")
-        self.assertEquals(len(sudoku_puzzle), 81)
+        solution = self.sudoku.parse_csv("solution.csv")
+        self.assertEquals(len(solution), 81)
 
     def test_solution_is_correct(self):
         solution = self.sudoku.parse_csv("solution.csv")
@@ -36,8 +35,7 @@ class SudokuTest(unittest.TestCase):
         self.assertTrue(puzzle_file.endswith("csv"))
 
     def test_elements_at_a_position(self):
-        sudoku_puzzle = self.sudoku.parse_csv("puzzle.csv")
-        matrix = self.sudoku.group(sudoku_puzzle, 9)
+        matrix = self.sudoku.group(self.sudoku_puzzle, 9)
         location = (0, 0)
 
         col_elements = self.sudoku.get_col(matrix, location)
@@ -49,21 +47,18 @@ class SudokuTest(unittest.TestCase):
         self.assertEquals(len(block_elements), 9)
 
     def test_input_file_is_not_already_solved(self):
-        sudoku_num = self.sudoku.parse_csv("puzzle.csv")
-        matrix = self.sudoku.group(sudoku_num, 9)
+        matrix = self.sudoku.group(self.sudoku_puzzle, 9)
         pos = self.sudoku.find_unsolved_location(matrix)
         self.assertIsInstance(pos, tuple)
 
     def test_possible_value_is_correct(self):
-        sudoku_num = self.sudoku.parse_csv("puzzle.csv")
-        matrix = self.sudoku.group(sudoku_num, 9)
+        matrix = self.sudoku.group(self.sudoku_puzzle, 9)
         pos = (0, 0)
         value = list(self.sudoku.find_possible_values(matrix, pos))
         self.assertEquals(value[0], 1)
 
     def test_value_is_legal(self):
-        sudoku_num = self.sudoku.parse_csv("puzzle.csv")
-        matrix = self.sudoku.group(sudoku_num, 9)
+        matrix = self.sudoku.group(self.sudoku_puzzle, 9)
         pos = (0, 0)
         self.assertTrue(self.sudoku.is_legal(1, matrix, pos))
 
